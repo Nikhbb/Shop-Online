14a84dd1b65d03cf15cb60ec4deda169825bb3ed
vnovikov <nik.hbb@gmail.com>
2024-02-03 10:17:23 +0200

    Implement view

--- src/app/view/OrderView.java
+++ src/app/view/OrderView.java
@@ -1,4 +1,13 @@
 package app.view;
 
+import app.model.IOrder;
+
 public class OrderView {
+
+    public void printOrderInfo(String productName, int productCount, double productPrice, boolean isDelivery, double total){
+        System.out.print("\nYour order INFO:");
+        System.out.print(productName + " " + productCount + " units * $" + productPrice);
+        System.out.print("is delivery: " + isDelivery);
+        System.out.print("TOTAL: " + total);
+    }
 }
